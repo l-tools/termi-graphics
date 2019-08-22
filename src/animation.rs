e91acd42885f950608e17659fad45bd6c41ef1d0
guy <guy123levinger@gmail.com>
2019-08-21 21:48:16 -0400

    animations

--- src/animation.rs
+++ src/animation.rs
@@ -4,3 +4,29 @@ use crate::ubuntu_terminal_sceme::*;
 use crate::ubuntu_terminal_sceme::*;
 #[cfg(target_os = "windows")]
 use crate::windows_cmd_sceme::*;
+
+use crate::pixel_art::Screen;
+use std::{thread, time};
+
+pub struct Animation{
+    roll:Vec<Screen>,
+    fps:u8,
+    length:u8,
+}
+impl Animation{
+    pub fn new(roll:Vec<Screen>,fps:u8,length:u8)->Animation{
+        let an1 = Animation{roll,fps,length};
+        an1
+    }
+    pub fn play(&self){
+        for i in 0..(self.fps*self.length/(self.roll.len() as u8)){
+            self.roll[i as usize].print_screen();
+            print!("\x1B[2J");
+            thread::sleep(time::Duration::from_millis(1000/self.fps as u64));
+        }
+        
+    }
+    pub fn pause(&self){
+        print!("\u{1B}[?25l");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod pixel_art;
 mod color_string;
 mod ubuntu_terminal_sceme;
 mod windows_cmd_sceme;
+mod animation;
 //use pixel_art::{PixelColors,Screen};
 //use std::{thread, time};
 //fn main() {
